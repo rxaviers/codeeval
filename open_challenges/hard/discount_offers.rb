d34cc5b7498e165da5a1c99bfc50d875f36c91f4
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-02-04 04:06:30 -0200

    Discount Offers: Making biggest_SS algorithm more accurate

--- open_challenges/hard/discount_offers.rb
+++ open_challenges/hard/discount_offers.rb
@@ -2,9 +2,10 @@
 # CodeEval - Hard 10) Discount Offers
 # http://www.codeeval.com/open_challenges/48/
 # author: Rafael Xavier <rxaviers at gmail.com>
-# NOT COMPLETE
+# score: 83.33 (arg! almost)
 
-VOWELS = %w{a e i o u}
+QUALITY = :perfect            # :fast or :perfect
+VOWELS = %w{a e i o u y}
 DEBUG = false
 
 def SS(customer, product)
@@ -29,35 +30,61 @@ def biggest_SS(ss_table)
     # For each product, get the most valuable SS. How? Give it to a customer
     # whom if you had given any other product, it wouldn't make that much value.
     #
-    # Let's begin with the first product.
+    # Let's begin with the first product. For each customer, we calculate the ss
+    # difference of giving him this product versus all other products. Then,  we
+    # get the min of those differences. That's the same as getting the
+    # difference from the first_product_ss to other_sses_max. With all the
+    # customers' deltas in hand we are able to conclude which customer should
+    # stay with the product.  It's the one whose benefit will be bigger (max).
+    # The bigger the delta_ss, the bigger the value of giving this product to
+    # customer_i.
 
-    # Many customers
+    # On many customers
     if ss_table.length > 1
-        # Many products
+        # On many products
         if ss_table[0].length > 1
-            # For each customer, get the customer with max delta_ss
-            max_delta_ss_customer_i = ss_table.map {|customer_sses|
+            delta_sses = ss_table.map {|customer_sses|
                 first_product_ss = customer_sses[0]
-                other_max_ss = customer_sses[1..-1].max
-                delta_ss = first_product_ss - other_max_ss
-            }.each_with_index.max[1]
-
-            # The remaining_ss_table is the ss_table without the customer row and
-            # without the product column.
-            remaining_ss_table = []
-            (0..ss_table.length-1).each {|customer_i|
-                remaining_ss_table<< ss_table[customer_i][1..-1] \
-                    unless customer_i == max_delta_ss_customer_i
+                other_sses_max = customer_sses[1..-1].max
+                delta_ss = first_product_ss - other_sses_max
             }
+            if QUALITY == :fast
+                max_delta_ss_customer_i = delta_sses.each_with_index.max[1]
 
-            ss_table[max_delta_ss_customer_i][0] + biggest_SS(remaining_ss_table)
+                # The remaining_ss_table is the ss_table without the customer row a
+                # without the product column.
+                remaining_ss_table = []
+                (0..ss_table.length-1).each {|customer_i|
+                    remaining_ss_table<< ss_table[customer_i][1..-1] \
+                        unless customer_i == max_delta_ss_customer_i
+                }
 
-        # One product
+                ss_table[max_delta_ss_customer_i][0] + biggest_SS(remaining_ss_table)
+            elsif QUALITY == :perfect
+                max_delta_ss = delta_sses.max
+                max_delta_ss_customers_is = delta_sses.each_with_index.select {|v,k|
+                    v == max_delta_ss
+                }.transpose[1]
+
+                # In case we have more than one max_delta_ss, try them all:
+                max_delta_ss_customers_is.map {|max_delta_ss_customer_i|
+                    # The remaining_ss_table is the ss_table without the customer row
+                    # and without the product column.
+                    remaining_ss_table = []
+                    (0..ss_table.length-1).each {|customer_i|
+                        remaining_ss_table<< ss_table[customer_i][1..-1] \
+                            unless customer_i == max_delta_ss_customer_i
+                    }
+
+                    ss_table[max_delta_ss_customer_i][0] + biggest_SS(remaining_ss_table)
+                }.max
+            end
+        # On one product
         else
             # Return the biggest ss left
             ss_table.transpose[0].max
         end
-    # One customer
+    # On one customer
     else
         ss_table[0][0]
     end
@@ -82,8 +109,12 @@ class Test
 end
 
 class String
+    def letters?
+        self.downcase =~ /[a-z]/ ? true : false
+    end
+
     def letters
-        chars.to_a.select {|c| c.downcase =~ /[a-z]/}
+        chars.to_a.select {|c| c.letters?}
     end
 
     def vowels
@@ -91,7 +122,7 @@ class String
     end
 
     def consonants
-        chars.to_a.select {|c| not VOWELS.include?(c.downcase) and c != ' '}
+        chars.to_a.select {|c| not VOWELS.include?(c.downcase) and c.letters?}
     end
 end
 
